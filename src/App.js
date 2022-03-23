88e733326ec29e81923dc56092b9910e331a21cc
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-23 13:04:32 -0500

    code commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+
+import React,{useState} from "react"
+
+
+
+import "./App.css"
+function App (){
+
+// state variable
+
+const[inputList,  setInputList]= useState("");
+const[items,setItems]=useState([])
+
+
+
+const itemEvents=(e)=>{
+  setInputList(e.target.value)
+}
+
+
+const listOfItems=()=>{
+setItems((items)=>{
+  return[...items,inputList]
+})
+setInputList("");
+}
+
+
+
+  return (
+<div>
+    <div className="main_div">
+      <div className="center_div">
+        <br/>
+        <h1>ToDo List</h1>
+          <br/>
+          <input type="text" placeholder="Add Items" value={inputList} onChange={itemEvents}/>
+          <button onClick={listOfItems}>+</button>
+          <ol>
+           { items.map((val)=>{
+             return <li>{val}</li>
+            })}
+          </ol>
+      </div>
+      
+      </div> 
+  </div>          /*main div ending */
+
+
+
+  );
+}
+
+export default App
+
+
+
