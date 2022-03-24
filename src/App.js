fd02167739f026362851a53ddcbec9b641bbaa70
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-24 12:30:56 -0500

    comment added

--- src/App.js
+++ src/App.js
@@ -1,67 +1,72 @@
+import React, { useState } from "react";
 
-import React,{useState} from "react";
-
-import "./App.css"
+import "./App.css";
 import TodoList from "./TodoList";
-function App (){
+function App() {
+  // state variable
 
-// state variable
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
 
-const[inputList,  setInputList]= useState("");
-const[items,setItems]=useState([])
+  const itemEvents = (e) => {
+    setInputList(e.target.value);
+  };
 
+  const listOfItems = () => {
+    setItems((items) => {
+      return [...items, inputList];
+    });
+    setInputList("");
+  };
 
+  const deleteItems = (id) => {
+    console.log("deleted");
 
-const itemEvents=(e)=>{
-  setInputList(e.target.value)
-}
-
-
-const listOfItems=()=>{
-setItems((items)=>{
-  return[...items,inputList]
-})
-setInputList("");
-}
-
-
-const deleteItems=(id)=>{
-  console.log("deleted");
-
-  setItems((items)=>{
-    return items.filter((elem,index)=>{
-      return index!==id;
-    })
-  })
-}
-
-
+    setItems((items) => {
+      return items.filter((elem, index) => {
+        return index !== id;
+      });
+    });
+  };
 
   return (
-<div>
-    <div className="main_div">
-      <div className="center_div">
-        <br/>
-        <h1>ToDo List</h1>
-          <br/>
-          <input type="text" placeholder="Add Items" value={inputList} onChange={itemEvents}/>
+    <div>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add Items"
+            value={inputList}
+            onChange={itemEvents}
+          />
           <button onClick={listOfItems}>+</button>
           <ol>
-           { items.map((val,index)=>{
-              return <TodoList key={index}  id={index} text={val} onSelect={deleteItems}/>
+            {items.map((val, index) => {
+              return (
+                <TodoList
+                  key={index}
+                  id={index}
+                  text={val}
+                  onSelect={deleteItems}
+                />
+              );
             })}
           </ol>
+        </div>
       </div>
-      
-      </div> 
-  </div>          /*main div ending */
-
-
-
+    </div> /*main div ending */
   );
 }
 
-export default App
+export default App;
 
+// return <li>{val}</li>
 
-
+// {/* <ol>
+//            { items.map((val)=>{
+//              return <li>{val}</li>
+//             })}
+//           </ol> */}
